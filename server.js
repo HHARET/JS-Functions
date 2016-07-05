612d902e361c2e251d415df595657b5809260347
Tarek H <mercabien@gmail.com>
2016-07-05 19:10:24 -0400

    double check

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var hostname = '127.0.0.1'
-var port=5000;
+var port=process.env.PORT || 5000;
 http.createServer(function(request,response){
   response.writeHead(200,{'Content-Type':'test/plain'});
   response.end("Hello");
