9f817447fde9e50d7a849737f7cd261bd4c0aa19
Tarek H <mercabien@gmail.com>
2016-07-05 19:16:53 -0400

    fixed the server.js Content-type

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var port=process.env.PORT || 5000;
 http.createServer(function(request,response){
-  response.writeHead(200,{'Content-Type':'test/plain'});
+  response.writeHead(200,{'Content-Type':'text/plain'});
   response.end("Hello");
 }).listen(port);
