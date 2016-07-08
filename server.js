572c9de57c21b17380ed9d41f4fc783164ee59f4
Tarek <tarek_s_a@yahoo.com>
2016-07-08 15:32:18 -0430

    changed the server message

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 var port=process.env.PORT || 5000;
 http.createServer(function(request,response){
   response.writeHead(200,{'Content-Type':'text/plain'});
-  response.end("Hello");
+  response.end("Tarek test app");
 }).listen(port);
